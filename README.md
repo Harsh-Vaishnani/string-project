84d3c4d4f7101c5b288f0f0b8702fc559f85ef91
Harsh-Vaishani <harshvaishnani8@gmail.com>
2024-01-19 11:38:57 +0530

    string project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # string-project
+![Alt text](strexm-1.png)
+![Alt text](STR-2.png)
+![Alt text](str-3.png)
+![Alt text](str-4.png)
---
+++ index.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    // *******************************************************QUE-1
+    
+    // char str1[100];
+    // printf("Enter the First Name ");
+    // scanf("%s",&str1);
+    // char str2[100];
+    //  printf("Enter the Second Name ");
+    // scanf("%s",&str2);
+    
+    // int id=strcmp(str1,str2);
+    // if(id==0){
+    //     printf("Both Are Equal");
+    // }
+    // else{
+    //     printf("Not Equal");
+    // }   
+        
+        
+    // ************************************************************QUE-2  
+    
+    // char str3[6]="zabcxy";
+    // for(int i=5; i>=0; i--){
+    //     printf("%c\n",str3[i]);
+    // }
+    
+    // ********************************************************QUE-3  
+    
+    // char str3[100]="xyz";
+    // char str4[100]="abc";
+    // printf("%s",strcat(str3,str4));
+    
+    
+    // *************************************************QUE-4
+    
+    //  char str5[100];
+    // printf("Enter Your Name ");
+    // scanf("%s",&str5);
+    // for(int i=0; str5[i]>0; i++){
+    //     printf("%c\n",str5[i]);
+    // }
+             
+    return 0;
+}
